59371c529fb67756ab0080c638c0fa2a1d1c7add
Carolina Ferreiro Belenguer <88061350+cferreirobelenguer@users.noreply.github.com>
2022-12-09 09:19:03 +0100

    componente personajes

--- src/app/dbz/dbz.module.ts
+++ src/app/dbz/dbz.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import {CommonModule} from '@angular/common';
 import { DbzComponent } from './dbz.component';
 import { FormsModule,ReactiveFormsModule } from '@angular/forms';
+import { PersonajesComponent } from './personajes/personajes.component';
 
 //Componentes importados en module
 
 @NgModule({
   declarations: [
-    DbzComponent
+    DbzComponent,
+    PersonajesComponent
   ],
   exports:[
     DbzComponent
